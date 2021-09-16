a4fe74f15cb80cce71c3a8a4a48cad205ae4f7cd
Natthawat <35883835+ProfileNatthawat@users.noreply.github.com>
2021-09-16 19:47:46 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,12 +11,7 @@ import { BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 
 function App() {
   const design = {color:'red',textAlign:"center",fontSize:"1.5rem"}
-  const initState = [
-    {id: 1,title:"ค่าเช่าบ้าน",amount:-2000},
-    {id: 2,title:"เงินเดือน",amount:12000},
-    {id: 3,title:"ค่าเดินทาง",amount:-500},
-    {id: 4,title:"ขายของ",amount:2000},
-  ]
+  const initState = []
   const [items,setItem] = useState(initState)
 
   const [reportincome,setReportincome] = useState(0)
